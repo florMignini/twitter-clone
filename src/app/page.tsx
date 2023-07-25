8951218e49390839d2fcf1ec1005fbfb8b662d5e
florMignini <flormignini29@gmail.com>
2023-07-25 08:55:10 -0300

    leftside UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
-import Link from "next/link";
-import {BsTwitter} from 'react-icons/bs'
-import { nav_items } from "../../data/navigation-items";
+import { LeftSidebar } from "@/components/LeftSidebar";
+
 
 export default function Home() {
   return (
    <div className="w-full h-full justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative px-12">
         {/* Sidebar and notification section */}
-        <section className="fixed w-62 flex flex-col h-screen my-4 text-xl space-y-4">
-            <Link href={'/'} className="px-3 text-3xl">
-                <BsTwitter/>
-            </Link>
-            {
-              nav_items.map((nav_item)=>(
-                <Link className="flex items-center w-fit justify-start space-x-5 rounded-xl px-3 py-2"
-                href={`${nav_item.title.toLowerCase()}`}
-                key={nav_item.title}
-                >
-                <div className="text-3xl">
-                  <nav_item.icon/>
-                </div>
-                <div>{nav_item.title}</div>
-                </Link>
-              ))
-            }
-            <button className="w-full rounded-3xl bg-blue-500 py-2 text-xl hover:bg-opacity-70 transition duration-200">
-              Tweet
-            </button>
-        </section>
+        <LeftSidebar/>
         {/* 
         <main>
         Main content ---> Twitts
---
+++ src/components/LeftSidebar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import {BsThreeDots, BsTwitter} from 'react-icons/bs'
+import { nav_items } from "../../data/navigation-items";
+
+export const LeftSidebar = () => {
+  return (
+    <section className="fixed w-62 h-screen flex flex-col items-stretch justify-between text-xl">
+   <div className="flex flex-col items-stretch justify-start">
+   <Link href={'/'} className="px-3 text-3xl mb-1 mt-2">
+        <BsTwitter/>
+    </Link>
+    {
+      nav_items.map((nav_item)=>(
+        <Link className="hover:bg-white/10 flex items-center w-fit h-fit justify-start space-x-5 rounded-3xl px-3 py-3"
+        href={`${nav_item.title.toLowerCase()}`}
+        key={nav_item.title}
+        >
+        <div className="text-3xl">
+          <nav_item.icon/>
+        </div>
+        <div>{nav_item.title}</div>
+        </Link>
+      ))
+    }
+    <button className="w-full rounded-3xl bg-blue-500 py-2 text-xl hover:bg-opacity-70 transition duration-200 my-4">
+      Tweet
+    </button>
+   </div>
+   <button className="w-full flex items-center justify-around space-x-2 rounded-full py-2 hover:bg-white/10
+   transition duration-200 p-1 my-4">
+    <div className="rounded-full bg-slate-400 w-10 h-10">{/* Avatar */}</div>
+    <div className="flex flex-col text-xs text-left">
+      <div className="text-semibold">La_Florineta</div>
+      <div>@mariflor_la</div>
+    </div>
+    <div className="text-sm text-right">
+      <BsThreeDots/>
+    </div>
+   </button>
+</section>
+  )
+}
