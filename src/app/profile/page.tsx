28a5b0f61b62f5661a659a3c48e6d6745fac56fb
Florencia Mignini <flormignini@Mac-mini-de-Florencia.local>
2023-08-29 16:31:01 -0300

    setting home as initial page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,17 @@
+import axios from 'axios'
 import React from 'react'
 
 const profile = () => {
+    
+    const getUserSessionData = async() =>{
+       const userData = await axios.get('/api/users/profile')
+       console.log(userData.data)
+    }
   return (
     <div
     className='w-full h-screen bg-black'
     >
-        
+
     </div>
   )
 }
