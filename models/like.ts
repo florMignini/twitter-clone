bb9e064f11002d8fedfbdd887565504a55469439
florMignini <flormignini29@gmail.com>
2023-09-05 19:28:43 -0300

    bringing tweets

--- models/like.ts
+++ models/like.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const likeSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  tweet: { type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  tweet: { type: mongoose.Schema.Types.ObjectId, ref: "Tweet", required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
-const Like = mongoose.model('Like', likeSchema);
+const Like = mongoose.models.Like || mongoose.model("Like", likeSchema);
 
 export default Like;
--- models/tweet.ts
+++ models/tweet.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
+import User from "./user";
 
 const tweetSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  image: { type: String},
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Like' }],
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  image: { type: String },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Like" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   timestamp: { type: Date, default: Date.now },
 });
 
-const Tweet = mongoose.models.Tweet || mongoose.model('Tweet', tweetSchema);
+const Tweet = mongoose.models.Tweet || mongoose.model("Tweet", tweetSchema);
 export default Tweet;
