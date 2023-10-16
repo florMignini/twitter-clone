a05b584ce50a865889c1e229bda3d26cc7fdfcc8
florMignini <flormignini29@gmail.com>
2023-10-16 15:44:21 -0300

    EROFS

--- src/app/api/users/imageProfile/route.ts
+++ src/app/api/users/imageProfile/route.ts
@@ -1,11 +1,12 @@
 import { connectDB } from "@/db/config";
 import Tweet from "../../../../../models/tweet";
-import { unlink, writeFile, mkdtemp } from "fs/promises";
+import { unlink, writeFile} from "fs/promises";
 import { v2 as cloudinary } from "cloudinary";
 import { tmpdir } from 'node:os';
 import { NextRequest, NextResponse } from "next/server";
-import path, { join } from "path";
+
 import User from "../../../../../models/user";
+import path from "path";
 
 connectDB();
 
@@ -27,8 +28,8 @@ export const POST = async (req: any) => {
     const bytes = await imageToStorage.arrayBuffer();
     const buffer = Buffer.from(bytes);
     //keep in file system for preview
-    // const filePath = path.join(process.cwd(), tmpdir(), imageToStorage.name);
-    const filePath = await mkdtemp(join(tmpdir(), imageToStorage.name))
+    const filePath = path.join(process.cwd(), tmpdir(), imageToStorage.name);
+
     await writeFile(filePath, buffer);
     //upload to cloudinary
     const imageUploaded = await cloudinary.uploader.upload(filePath);
