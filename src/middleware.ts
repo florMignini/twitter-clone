2d14f11a59d4024a411936829e42891a29bf951c
florMignini <flormignini29@gmail.com>
2023-10-16 20:32:32 -0300

    cookies bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,5 +22,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/signin", "/signup", "/profile/:path*"],
+  matcher: ["/", "/signin", "/signup", "/profile/:path*", "/logout"],
 };
