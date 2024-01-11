c6acc0e8c81899aa3f6690ac88e7c8346a665de1
florMignini <flormignini29@gmail.com>
2024-01-11 08:30:32 -0300

    socket io connection rearrange

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,22 +1,3 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
 
-export const Messages = () => {
-  const socket = io("http://localhost:5000");
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log(socket.id);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  return(
-    <h1>Messages</h1>
-    )
-};
