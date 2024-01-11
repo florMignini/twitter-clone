9a0528187e0ae8ff71a8b2079ad43431dbd7f150
florMignini <flormignini29@gmail.com>
2024-01-11 10:20:56 -0300

    socket successfully running

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,11 +1,19 @@
 "use client";
-
+import { useSocket } from '@/providers/socket-provider';
 import React from 'react'
 
 const Messages = () => {
-  return (
-    <div>Messages</div>
-  )
+    const {isConnected} = useSocket();
+
+ if(!isConnected){
+    return (
+    <h1 className='text-6xl text-red-600'> Not Connected</h1>
+        )
+}
+
+return (
+    <h1 className='text-6xl text-emerald-600'> Connected</h1>
+        )
 }
 
 
