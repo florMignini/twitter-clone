88a4318ed176dad57f1b735c325565603cdf6a10
florMignini <flormignini29@gmail.com>
2024-01-15 17:42:38 -0300

    cookie bug fixed

--- src/helpers/getUserData.ts
+++ src/helpers/getUserData.ts
@@ -1,33 +1,33 @@
+"use server";
 import { NextApiRequest } from "next";
+import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
 export const getUserData = (request: NextApiRequest) => {
+  try {
+    const cookieStore = cookies();
 
+    const sessionToken = cookieStore.get("sessionToken")?.value;
 
-    try {
+    //google session
+    const googleToken = cookieStore.get("googleSessionToken")?.value;
 
-      const sessionToken = request?.cookies?.get("sessionToken")?.value;
-      //google session
-      const googleToken = request?.cookies?.get("googleSessionToken")?.value;
+    if (sessionToken) {
+      const decodedToken: any = jwt.verify(
+        sessionToken,
+        process.env.TOKEN_SECRET!
+      );
+      return decodedToken;
+    }
+    if (googleToken) {
+      const decodedGoogleToken: any = jwt.verify(
+        googleToken,
+        process.env.TOKEN_SECRET!
+      );
 
-      if (sessionToken) {
-        const decodedToken: any = jwt.verify(
-          sessionToken,
-          process.env.TOKEN_SECRET!
-        );
-        return decodedToken;
-      }
-      if (googleToken) {
-        const decodedGoogleToken: any = jwt.verify(
-          googleToken,
-          process.env.TOKEN_SECRET!
-        );
-
-        return decodedGoogleToken;
-        }
-   } catch (error: any) {
-     throw new Error(error.message);
-   }
-  
-  
+      return decodedGoogleToken;
+    }
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
