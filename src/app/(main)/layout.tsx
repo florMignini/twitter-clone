eaa7e7e1d0774e88585c4681f3c1a47a378aeaad
florMignini <flormignini29@gmail.com>
2024-02-22 10:03:31 -0300

    layout bug fixed

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
 }) {
   return (
     <TweetProvider>
-      <div className="p-0 w-full h-screen grid grid-cols-[15%,85%] md:grid-cols-[19%,70%,20%] relative md:px-10 2xl:px-72">
+      <div className="p-0 w-full h-screen grid grid-cols-[15%,85%] md:grid-cols-[19%,70%,20%] lg:grid-cols-[19%,50%,30%] relative md:px-10 2xl:px-72">
       {/* Sidebar and notification section  */}
       <div className="w-[100%] border-r-1 flex items-center justify-center border-zinc-700">
         <LeftSidebar />
