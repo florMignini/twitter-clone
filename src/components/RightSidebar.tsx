7d67428092be0d3a72ee137b5bff9911ef37c7f0
florMignini <flormignini29@gmail.com>
2024-04-01 06:45:42 -0300

    array suggestions

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -5,6 +5,7 @@ import { Search } from ".";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 type userType = {
   _id: string;
@@ -19,7 +20,7 @@ type userType = {
 const RightSidebar = () => {
   //bringing user session data && login session
   const userQuery = useGetSessionData();
-
+  const [fullArray, setFullArray] = useState<boolean>(false);
   //get all Users
   const { data, isLoading } = useGetUsers();
 
@@ -33,10 +34,13 @@ const RightSidebar = () => {
       return user._id;
     });
 
-  const suggestionsArray = followingList?.filter(
+  const fullSuggestionsArray = followingList?.filter(
     (user: userType) => !userList?.includes(user?._id)
   );
-// TODO: create a 3 element array list for suggestions and extend list button for get the full list of suggestions
+  const suggestionsArray = followingList
+    ?.filter((user: userType) => !userList?.includes(user?._id))
+    .slice(0, 3);
+  
   //Follow action
   const follow = async (followId: string, userId: string) => {
     await axios.post("/api/users/following", { followId, userId });
@@ -58,43 +62,81 @@ const RightSidebar = () => {
         <div className="w-fill flex flex-col rounded-2xl  bg-[#16181C] mt-2">
           <h3 className="text-xl font-bold py-3 px-4">Who to Follow</h3>
           <div>
-            {suggestionsArray?.map((suggestion: userType) => (
-              <button
-                className="w-full my-2 flex items-center justify-between px-3 py-1 hover:bg-white/10
-                      transition duration-200"
-                key={suggestion._id}
-              >
-                <div className="w-[70%] flex items-center justify-start">
-                  <Image
-                    width={50}
-                    height={50}
-                    className="rounded-full flex items-center justify-center"
-                    alt="userAvatar"
-                    src={suggestion?.imageUrl}
-                  />
-                  <div className="flex flex-col items-center justify-start w-[100%] truncate pl-2">
-                    <p className="w-[100%] text-sm font-thin flex items-center justify-start">
-                      {`${suggestion?.username}`}
-                    </p>
-                    <Link
-                      className="w-[100%] text-sm text-blue-300 flex items-center justify-start"
-                      href={`/profile?profileId=${suggestion?._id}`}
+            {followingList && fullArray
+              ? fullSuggestionsArray?.map((suggestion: userType) => (
+                  <button
+                    className="w-full my-2 flex items-center justify-between px-3 py-1 hover:bg-white/10
+                        transition duration-200"
+                    key={suggestion._id}
+                  >
+                    <div className="w-[70%] flex items-center justify-start">
+                      <Image
+                        width={50}
+                        height={50}
+                        className="rounded-full flex items-center justify-center"
+                        alt="userAvatar"
+                        src={suggestion?.imageUrl}
+                      />
+                      <div className="flex flex-col items-center justify-start w-[100%] truncate pl-2">
+                        <p className="w-[100%] text-sm font-thin flex items-center justify-start">
+                          {`${suggestion?.username}`}
+                        </p>
+                        <Link
+                          className="w-[100%] text-sm text-blue-300 flex items-center justify-start"
+                          href={`/profile?profileId=${suggestion?._id}`}
+                        >
+                          {`@${suggestion?.username}`}
+                        </Link>
+                      </div>
+                    </div>
+                    <button
+                      className="w-1/3 flex items-center justify-center px-5 py-1 bg-white rounded-2xl text-sm font-semibold text-black hover:opacity-80 lg:mb-2 "
+                      onClick={() => follow(suggestion?._id, userQuery?._id)}
                     >
-                      {`@${suggestion?.username}`}
-                    </Link>
-                  </div>
-                </div>
-                <button
-                  className="w-1/3 flex items-center justify-center px-5 py-1 bg-white rounded-2xl text-sm font-semibold text-black hover:opacity-80 lg:mb-2 "
-                  onClick={() => follow(suggestion?._id, userQuery?._id)}
-                >
-                  Follow
-                </button>
-              </button>
-            ))}
-            <h4 className="text-md text-blue-400 p-4 cursor-pointer hover:bg-black/30 last:rounded-b-2xl">
-              Show more
-            </h4>
+                      Follow
+                    </button>
+                  </button>
+                ))
+              : suggestionsArray?.map((suggestion: userType) => (
+                  <button
+                    className="w-full my-2 flex items-center justify-between px-3 py-1 hover:bg-white/10
+                      transition duration-200"
+                    key={suggestion._id}
+                  >
+                    <div className="w-[70%] flex items-center justify-start">
+                      <Image
+                        width={50}
+                        height={50}
+                        className="rounded-full flex items-center justify-center"
+                        alt="userAvatar"
+                        src={suggestion?.imageUrl}
+                      />
+                      <div className="flex flex-col items-center justify-start w-[100%] truncate pl-2">
+                        <p className="w-[100%] text-sm font-thin flex items-center justify-start">
+                          {`${suggestion?.username}`}
+                        </p>
+                        <Link
+                          className="w-[100%] text-sm text-blue-300 flex items-center justify-start"
+                          href={`/profile?profileId=${suggestion?._id}`}
+                        >
+                          {`@${suggestion?.username}`}
+                        </Link>
+                      </div>
+                    </div>
+                    <button
+                      className="w-1/3 flex items-center justify-center px-5 py-1 bg-white rounded-2xl text-sm font-semibold text-black hover:opacity-80 lg:mb-2 "
+                      onClick={() => follow(suggestion?._id, userQuery?._id)}
+                    >
+                      Follow
+                    </button>
+                  </button>
+                ))}
+            <button
+              onClick={() => setFullArray(!fullArray)}
+              className="w-full text-md text-blue-400 p-4 cursor-pointer hover:bg-black/30 last:rounded-b-2xl"
+            >
+              {fullArray ? <h4> Show less</h4> : <h4> Show more</h4>}
+            </button>
           </div>
         </div>
       )}
