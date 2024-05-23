84c59030abe57539f56bcfd4fbe6051cf7f612ed
florMignini <flormignini29@gmail.com>
2024-05-23 06:47:49 -0300

    update edgestore file

--- src/lib/edgestore.ts
+++ src/lib/edgestore.ts
@@ -3,7 +3,10 @@
 import { type EdgeStoreRouter } from '../app/api/edgestore/[...edgestore]/route';
 import { createEdgeStoreProvider } from '@edgestore/react';
  
-const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>();
+export async function generateStaticParams(){
+
+  const { EdgeStoreProvider, useEdgeStore } =
+    createEdgeStoreProvider<EdgeStoreRouter>();
+}
  
-export { EdgeStoreProvider, useEdgeStore };
+// export { EdgeStoreProvider, useEdgeStore };
